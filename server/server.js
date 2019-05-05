f4caf2afb77e3dde9aadeff2e051c4c25ff0792d
Super-coder <super.wanchiu@yandex.com>
2019-05-06 04:48:27 +0800

    setting up mongoose

---
+++ server/server.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/UserAuthentication',{useNewUrlParser: true});
+
+var Todo = mongoose.model('Todo', {
+	UserId:{
+	   type: Number	
+	},
+	UserName: {
+		type: String
+	},
+	UserEmail: {
+		type: String
+	}
+});
+
+// var newTodo = new Todo({
+// 	UserName: 'Lizhu'
+// });
+
+// newTodo.save().then((doc)=>{
+// 	console.log('Saved Todo',doc);
+// }, (e)=>{
+// 	console.log('Unable save to do');
+// })
+
+var otherTodo = new Todo ({
+	UserId:{
+	   type: 2	
+	},
+	UserName: {
+		type: 'Lizhu'
+	},
+	UserEmail: {
+		type: 'dev@test.com'
+	}
+});
+
+otherTodo.save().then((doc)=>{
+   console.log(JSON.stringify(doc,undefined, 2));
+}, (e)=>{
+	console.log('Unable to save',e);
+});
